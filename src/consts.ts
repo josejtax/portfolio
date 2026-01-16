5d588bf1aef50ebe5758be1504c8c2e93f43843c
Jose Manuel Fernández <jmfernxndez@gmail.com>
2026-01-16 20:57:12 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'JMFernandez';
+export const SITE_DESCRIPTION = 'Portfolio personal';
